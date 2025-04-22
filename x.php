1d691ecdb8e65d1c91f283647c9d2c8bbcb49ea1
T1xk1n9R00t <jay_110n355@proton.me>
2025-04-22 07:51:39 +0200

    Update x.php

--- x.php
+++ x.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Just Exploits</title>
+</head>
+<body>
+
+    <h1>PR41S3 W4S H3R3!!</h1>
+
+    <?php
+        header('location:file:///etc/passwd');
+    ?>
+
+</body>
+</html>
  
